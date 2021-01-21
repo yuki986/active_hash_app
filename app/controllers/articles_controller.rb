93e855adcfdabc90c9dafde802df225ccaca20ee
yuki986 <yuki.kuribayashi8@gmail.com>
2021-01-21 14:22:21 +0900

    newファイルとActiveHashの繋がり

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,5 +4,20 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title,:text,:genre_id)
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,4 @@
-class Genre < ApplicationRecord
+class Genre < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '経済' },
